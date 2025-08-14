190d7319f821e120ddfd1327f60ea762f197ff38
Hansel <fernandohanselsc@gmail.com>
2025-08-14 15:03:51 -0300

    feat: cria endpoint para cadastro de médicos com DTOs de dados, endereço e especialidade

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,2 +1,6 @@
-package med.voll.api.medico;public record DadosCadastroMedico() {
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 }
